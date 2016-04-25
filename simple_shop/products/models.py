9dece334b96e5c32544fa14d890bc08612e4c3d1
Alexander Voitenko <voitenko@qarea.com>
2016-04-25 20:19:39 +0300

    Added comments to product

--- simple_shop/products/models.py
+++ simple_shop/products/models.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
+from datetime import timedelta
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-
+from django.utils import timezone
 
 @python_2_unicode_compatible
 class Product(models.Model):
@@ -22,3 +24,21 @@ class Product(models.Model):
     def get_absolute_url(self):
         from django.core.urlresolvers import reverse
         return reverse('products:detail', args=[self.slug])
+
+    def last_comments(self):
+        period_limit = timezone.now()-timedelta(hours=24)
+        return self.productcomment_set.filter(created_at__gte=period_limit).order_by("-created_at")
+
+
+@python_2_unicode_compatible
+class ProductComment(models.Model):
+    product = models.ForeignKey(Product)
+    username = models.CharField(max_length=30, verbose_name='Your name')
+    text = models.TextField(verbose_name='Comment')
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'product_comments'
+
+    def __str__(self):
+        return '<%s: %s>' % (self.username, self.test[:30])
--- simple_shop/products/views.py
+++ simple_shop/products/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from . models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from django import forms
+from django.core.urlresolvers import reverse
+
+from . models import Product, ProductComment
+
+
+class ProductCommentForm(forms.ModelForm):
+    class Meta:
+        model = ProductComment
+        fields = ('username', 'text')
 
 
 def index(request):
@@ -7,4 +16,16 @@ def index(request):
 
 
 def detail(request, slug):
-    return render(request, 'product/detail.html', {'product': get_object_or_404(Product, slug=slug)})
+    product = get_object_or_404(Product, slug=slug)
+    if request.method == 'POST':
+        form = ProductCommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.product = product
+            comment.save()
+            return redirect(reverse('products:detail', args=[product.slug]) )
+    else:
+        form = ProductCommentForm()
+    return render(request, 'product/detail.html',
+                  {'product': product,
+                   'form': form})
